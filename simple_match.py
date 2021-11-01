7f850e3df909a6ff0b966dc6429776c1c97c4979
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2021-11-01 13:31:22 +0300

    Add files via upload

---
+++ simple_match.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Для каждого регулярного выражения, которое требуется написать,
+# указаны строки, соответствующие этому выражению (они отмечены знаком +),
+# а также строки, не соответствующие этому выражению (отмечены знаком -)
+
+# + a
+# + ab
+# - b
+# - ba
+REGEXP_1 = '^a'
+
+# + aab
+# + abb
+# + acb
+# - ab
+# - aabc
+REGEXP_2 = '[a-c]{3}$'
+
+# + sofia.mp3
+# + sofia.mp4
+# - sofia.mp7
+# - sofia.mp34
+REGEXP_3 = 'sofia.mp[34]$'
+
+# + taverna
+# + versus
+# + vera
+# + zveri
+# - zver
+REGEXP_4 = '^[a-zA-Z]*ver[a-zA-Z]+$'
+
+# - a
+# - aa
+# + aaa
+# - aaaa
+# - b
+# - bb
+# + bbb
+# - bbbb
+# - ccc
+REGEXP_5 = '[ab]{3}$'
+
+# - Ok
+# - OkOk
+# + OkOkOk
+# - OkOkOkOk
+# - ab
+# - abab
+# + ababab
+# - abababab
+REGEXP_6 = '(Ok){3}$|(ab){3}$'
+
+# - aaa
+# - aaa aaa
+# + aaa Aaa aaa
+# + aaa aaa Aaa
+# + Aaa aaa aaa
+# - A
+# - aaa A aaa
+REGEXP_7 = '[aA]{3} [aA]{3} [aA]{3}'
+
+# + abc
+# + abc03
+# + a-b-c-3
+# + a.b.c.0
+# - Aabc
+# - abc1
+# - #abc
+REGEXP_8 = 'abc$|abc03$|a-b-c-3$|a.b.c.0$'
