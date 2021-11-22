359d32c7576ffd7b18563b232e6580dce33f9dce
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2021-11-22 14:32:39 +0300

    Create Number2.py

---
+++ NumPy/Number2.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+def func1d(a):
+  return(a * weights)
+img = cv2.imread("pick.jpg")
+print(img.shape)
+weights = np.array([0.299, 0.587, 0.114])
+img2 = np.apply_along_axis(func1d, 2, img)
+img2 = np.sum(img2, axis=2)
+cv2.imwrite('grey_pick.jpg', img2)
